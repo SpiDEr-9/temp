d794d069d741ad0bc03312fa77458910911d8237
D3_62979_Bhushan <bhush50@gmail.com>
2022-06-20 11:43:06 +0530

    odd added

--- 1.js
+++ 1.js
@@ -8,3 +8,15 @@ function even(num){
 
     even(10)
     even(15)
+
+
+    function odd(num){
+
+        if(num%2!=0){
+            console.log("odd number");
+        }else{
+            console.log("even number");
+        }}
+    
+        even(10)
+        even(15)
