ae4b2abb438b03b00690739df201f7f429ed0a3d
D3_62979_Bhushan <bhush50@gmail.com>
2022-06-20 11:52:41 +0530

    squre

--- 1.js
+++ 1.js
@@ -32,4 +32,10 @@ function even(num){
 
         function odd(num){
 
-            if(num%2!=0){
+            if(num%2!=0){}}
+
+
+                function squre(num){
+
+                   console.log( num*num)}
+                   squre(2)
